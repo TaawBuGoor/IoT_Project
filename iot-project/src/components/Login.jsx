710e22ef73b18d49dd4010fdd3920cdf41a1162f
Rawane Diouf <rawanediouf6@gmail.com>
2024-06-16 01:28:44 +0000

    Admin Login in perfectionment :)

--- iot-project/src/components/Login.jsx
+++ iot-project/src/components/Login.jsx
@@ -1,35 +1,38 @@
 import '../styles/Login.css'
+import logo from '../assets/iot-key.jpg'
 
 function Login(){
     return(
-        <div className='login'>
+        <div className='login-main'>
             <div className='container'>
                 <div className='container_left'>
-                    <h1>IoT</h1>
+                    <div>
+                        <img src={logo} className='logo' alt='logo' />
+                    </div>
                 </div>
 
                 <div className='container_right'>
                     <h1>LogIn</h1>
-                    <div className='input_wrapper'>
-                        <input type='text' className='input' required />
-                        <span className='label'>Username *</span>
-                    </div>
-                    <div className='input_wrapper'>
-                        <input type='password' className='input' required />
-                        <span className='label'>Password *</span>
-                    </div>
-                    <div className='remember'>
-                        <div>
-                            <input type='checkbox' className='checkbox'/>
-                            <label for='text'>Remember me</label>
+                    <div className='login-box'>
+                        <div className='input_wrapper'>
+                            <input type='text' className='input' required />
+                            <span className='label'>Username *</span>
                         </div>
-                        <div>
+                        <div className='input_wrapper'>
+                            <input type='password' className='input' required />
+                            <span className='label'>Password *</span>
+                        </div>
+                        <div className='remember'>
+                            <div>
+                                <input type='checkbox' className='checkbox'/>
+                                <label for='text'>Remember me</label>
+                            </div>
                             <a href='me' className='forgot'>Forgot Password ?</a>
                         </div>
-                    </div>
-                    <button className='btn'>LogIn</button>
-                    <div className='registerLink'>
-                        <a href='me'>Create an account ?</a>
+                        <button className='btn'>LogIn</button>
+                        <div className='registerLink'>
+                            <a href='me'>Create an account ?</a>
+                        </div>
                     </div>
                 </div>
             </div>
